90694ec554e84de4a7869af1a8cbe9e142051d0c
Ruslan G <ruslanmgaifulin@gmail.com>
2026-08-30 11:48:54 +0300

    Clarify local PostgreSQL settings

--- deployment/client/create_local_db_in_docker.ps1
+++ deployment/client/create_local_db_in_docker.ps1
@@ -1,13 +1,24 @@
 $ErrorActionPreference = 'Stop'
 
-docker run --name gaifulinlab-postgres-dev `
-    --restart unless-stopped `
-    -e POSTGRES_DB=gaifulinlab `
-    -e POSTGRES_USER=gaifulinlab `
-    -e POSTGRES_PASSWORD=gaifulinlab `
-    -p '127.0.0.1:5432:5432' `
-    -v 'gaifulinlab-postgres-dev-data:/var/lib/postgresql/data' `
-    -d postgres:17-alpine
+$containerName = 'gaifulinlab-postgres-dev'
+$imageName = 'postgres:17-alpine'
+$databaseName = 'gaifulinlab'
+$databaseUser = 'gaifulinlab'
+$databasePassword = 'gaifulinlab'
+$hostAddress = '127.0.0.1'
+$postgresPort = '5432'
+$volumeName = 'gaifulinlab-postgres-dev-data'
+$postgresDataPath = '/var/lib/postgresql/data'
+$restartPolicy = 'unless-stopped'
+
+docker run --name $containerName `
+    --restart $restartPolicy `
+    -e "POSTGRES_DB=$databaseName" `
+    -e "POSTGRES_USER=$databaseUser" `
+    -e "POSTGRES_PASSWORD=$databasePassword" `
+    -p "${hostAddress}:${postgresPort}:${postgresPort}" `
+    -v "${volumeName}:${postgresDataPath}" `
+    -d $imageName
 
 if ($LASTEXITCODE -ne 0) {
     throw 'Could not create the local PostgreSQL container.'
